a946d58c9c44f24db3830a0d6fff85439a7065b0
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-22 04:30:03 +0200

    errors: create

---
+++ src/errors.ts
@@ -0,0 +1,10 @@
+import rorre from "rorre";
+
+export default rorre.declare({
+  // helpers/getRandomNumberBetween()
+  HPR_01_PRM_MAX_NOT_INT: "<max> must be an integer.",
+  HPR_01_PRM_MAX_NOT_POS: "<max> must be a positive integer.",
+  HPR_01_PRM_MAX_NOT_MRE: "<max> must be greater than <min>.",
+  HPR_01_PRM_MIN_NOT_INT: "<min> must be an integer.",
+  HPR_01_PRM_MIN_NOT_POS: "<min> must be a positive integer."
+});
