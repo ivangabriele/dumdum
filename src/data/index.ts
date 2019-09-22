7114c3caaff8d7d9a0988f4a759ed62a0f18f6f3
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-22 05:43:01 +0200

    data: index

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+// tslint:disable: import-name
+
+import enTextPlain from "./en/texts.json";
+import frTextPlain from "./fr/texts.json";
+
+export default {
+  en: { text: { plain: enTextPlain } },
+  fr: { text: { plain: frTextPlain } }
+};
