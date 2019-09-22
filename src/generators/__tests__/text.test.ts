fc286dfd4a0e8e15af37626b156065435a2411b7
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-22 06:13:59 +0200

    generators/text: fix tests

--- src/generators/__tests__/text.test.ts
+++ src/generators/__tests__/text.test.ts
@@ -3,17 +3,21 @@ import text from "../text";
 describe("generators/text()", () => {
   test("should return the expected result with a <maxLength> in EN", async () => {
     expect(text("en", 100).length).toBeGreaterThanOrEqual(12);
+    expect(text("fr", 100).length).toBeLessThanOrEqual(100);
   });
 
   test("should return the expected result with a <maxLength> in FR", async () => {
+    expect(text("fr", 100).length).toBeGreaterThanOrEqual(12);
     expect(text("fr", 100).length).toBeLessThanOrEqual(100);
   });
 
   test("should return the expected result with a <[minLength, maxLength]> in EN", async () => {
     expect(text("en", [50, 150]).length).toBeGreaterThanOrEqual(50);
+    expect(text("en", [50, 150]).length).toBeLessThanOrEqual(150);
   });
 
   test("should return the expected result with a <[minLength, maxLength]> in FR", async () => {
-    expect(text("fr", [50, 150]).length).toBeLessThanOrEqual(100);
+    expect(text("fr", [50, 150]).length).toBeGreaterThanOrEqual(50);
+    expect(text("fr", [50, 150]).length).toBeLessThanOrEqual(150);
   });
 });
