8345518d3bad6c9ca863a970c6d7151790f76d15
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-22 06:15:58 +0200

    generators/text: fix missing minimum text length calculation

--- src/generators/text.ts
+++ src/generators/text.ts
@@ -2,12 +2,14 @@ import data from "../data";
 import getRandomNumberBetween from "../helpers/getRandomNumberBetween";
 import { Locale, TextType } from "../types";
 
+const MIN_TEXT_LENGTH = 12;
+
 function text(locale: Locale, maxLength: number, type?: TextType): string;
 function text(locale: Locale, [minLength, maxLength]: [number, number], type?: TextType): string;
 function text(locale: Locale, length: number | [number, number], type: TextType = "plain"): any {
-  const minLength = Array.isArray(length) ? length[0] : 12;
+  const minLength = Array.isArray(length) ? length[0] : MIN_TEXT_LENGTH;
   const maxLength = Array.isArray(length) ? length[1] : length;
-  const index = getRandomNumberBetween(minLength, maxLength);
+  const index = getRandomNumberBetween(minLength, maxLength) - MIN_TEXT_LENGTH;
 
   return data[locale].text[type][index];
 }
