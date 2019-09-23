d0055d033ffcaab2fc7c6f54d7d8a0a270e515ab
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-09-23 05:24:57 +0200

    errors: update

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,22 @@
 import rorre from "rorre";
 
+import metaJson from "./meta.json";
+
+const MAX_TEXT_LENGTH = metaJson.maxTextLength;
+const MIN_TEXT_LENGTH = metaJson.minTextLength;
+
 export default rorre.declare({
+  // generators/text()
+  GNR_01_PRM_MAX_NOT_INT: "<maxLength> must be an integer.",
+  GNR_01_PRM_MAX_NOT_POS: "<maxLength> must be a positive integer.",
+  GNR_01_PRM_MAX_NOT_MRE: "<maxLength> must be greater than <minLength>.",
+  GNR_01_PRM_MAX_TOO_LOW: `<maxLength> must be greater than or equal to ${MIN_TEXT_LENGTH}.`,
+  GNR_01_PRM_MAX_TOO_HIG: `<maxLength> must be lower than or equal to ${MAX_TEXT_LENGTH}.`,
+  GNR_01_PRM_MIN_NOT_INT: "<minLength> must be an integer.",
+  GNR_01_PRM_MIN_NOT_POS: "<minLength> must be a positive integer.",
+  GNR_01_PRM_MIN_TOO_LOW: `<minLength> must be greater than or equal to ${MIN_TEXT_LENGTH}.`,
+  GNR_01_PRM_MIN_TOO_HIG: `<minLength> must be lower than or equal to ${MAX_TEXT_LENGTH}.`,
+
   // helpers/getRandomNumberBetween()
   HPR_01_PRM_MAX_NOT_INT: "<max> must be an integer.",
   HPR_01_PRM_MAX_NOT_POS: "<max> must be a positive integer.",
